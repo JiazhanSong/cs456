05d74d235f6168cb9876be131622b9466ecadad4
Jiazhan <jiazhan@Jiazhan-PC.localdomain>
2018-11-07 19:46:24 -0500

    a2

--- a2/sender.py
+++ a2/sender.py
@@ -34,10 +34,9 @@ with open(filename) as f:
   data = f.read()
 
 packets = []
-
-data = [data[ p:p + packet.MAX_DATA_LENGTH] for p in range(0, len(data), packet.MAX_DATA_LENGTH)]
-for p in range(len(data)):
-  packets.append( packet.create_packet( p, data[ p:(p + packet.MAX_DATA_LENGTH) ] ))
+for p in range(0, len(data), packet.MAX_DATA_LENGTH):
+  print(p/packet.MAX_DATA_LENGTH)
+  packets.append( packet.create_packet( int(p/packet.MAX_DATA_LENGTH), data[ p:(p + packet.MAX_DATA_LENGTH) ] ))
 
 
 N = 10              # window size
